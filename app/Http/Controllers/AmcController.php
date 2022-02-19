627cd3557b2459775aac5ae76c0694f054183338
Hamza Ellatif <hamza.ellatif@alphanetworks.tv>
2022-02-19 20:04:22 +0100

    commit all files

---
+++ app/Http/Controllers/AmcController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Amc;
+use Illuminate\Http\Request;
+
+class AmcController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Amc  $amc
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Amc $amc)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Amc  $amc
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Amc $amc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Amc  $amc
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Amc $amc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Amc  $amc
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Amc $amc)
+    {
+        //
+    }
+}
